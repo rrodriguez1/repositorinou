d7067c7a457ffbf959eb30a1bf431f9e82fe2969
Raquel <rrodriguez.1asmx@gmail.com>
2014-10-15 13:57:14 +0200

    primera subida

---
+++ FRASELETRAS.java
@@ -0,0 +1,49 @@
+/**
+ * paquetes importados.
+* */
+import java.util.Scanner;
+/**
+ * paquetes importados.
+ **/
+import java.util.ArrayList;
+/**
+ * @author raquel .
+ *
+ */
+public class FRASELETRAS {
+/**
+ * @param args
+ */
+public static void main(final String[] args) {
+		// TODO Auto-generated method stub
+		Scanner lector = new Scanner(System.in);
+		System.out.print("Escribe una frase: ");
+		ArrayList<Character> letras = new ArrayList<Character>();
+		String frase = lector.nextLine();
+		letras.add(frase.charAt(0));
+		boolean encontrada;
+		for (int i = 1; i < frase.length(); i++) {
+			char letra = frase.charAt(i);
+			encontrada = false;
+			for (int j = 0; j < letras.size(); j++) {
+				if (letra == letras.get(j)) {
+					encontrada = true;
+				}
+			}
+			if (encontrada != true) {
+				letras.add(letra);
+			}
+		}
+		int [] repeticiones = new int[letras.size()];
+		for (int i = 0; i < letras.size(); i++) {
+			for (int j = 0; j < frase.length(); j++) {
+				if (letras.get(i) == frase.charAt(j)) {
+					repeticiones[i]++;
+				}
+			}
+System.out.println("La letra " + letras.get(i)
++ " sale " + repeticiones[i] + " veces.");
+			}
+	}
+}
+
